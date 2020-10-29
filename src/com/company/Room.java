7e73cd54795fe0905b3532a4f9e51ade7abb5f68
Dragos-Nemes <dragos_nemes@yahoo.com>
2020-10-29 21:35:26 +0200

    Programs

---
+++ src/com/company/Room.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Room {
+    private int lenght;
+    private int height;
+
+    private int width;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(int lenght) {
+        this.lenght = lenght;
+    }
+}
